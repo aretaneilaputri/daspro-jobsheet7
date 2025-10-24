f7930be9287e6cd770dc5fd7f233d4ef50193173
“aretaneilaputri <retaaneila@gmail.com>
2025-10-24 13:24:56 +0700

    Modifikasi Percobaan 1 (Perintah nomor 4 dan 5)

--- SiakadFor03.java
+++ SiakadFor03.java
@@ -8,21 +8,36 @@ public class SiakadFor03 {
         double nilai;
         double tertinggi = 0;
         double terendah = 100;
+        int jumlahMahasiswa = 10;
+        int lulus = 0;
+        int tidakLulus = 0;
 
+         System.out.println("Masukkan nilai untuk " + jumlahMahasiswa + " mahasiswa:");
         for (int i = 1; i <= 10; i++) {
             System.out.print("Nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid, anggap 0 dan lanjutkan.");
+            }
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+             if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi = " + tertinggi);
         System.out.println("Nilai terendah  = " + terendah);
-
+        System.out.println("Jumlah lulus    = " + lulus);
+        System.out.println("Jumlah tidak lulus = " + tidakLulus);
+        
         sc.close();
     }
 }
